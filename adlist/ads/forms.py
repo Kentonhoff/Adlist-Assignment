5f7153a87881e800f23a4a65287f6820b6dea635
Kentonhoff <44037845+Kentonhoff@users.noreply.github.com>
2019-04-03 19:08:18 -0400

    Completed part 2. Still need to create favicon.

--- adlist/ads/forms.py
+++ adlist/ads/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from ads.models import Ad
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from ads.humanize import naturalsize
+from django.core.exceptions import ValidationError
+from django.core import validators
+
+class CommentForm(forms.Form):
+    comment = forms.CharField(required=True, max_length=500, min_length=3, strip=True)
 
 # https://docs.djangoproject.com/en/2.1/topics/http/file-uploads/
 # https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
@@ -21,7 +26,7 @@ class CreateForm(forms.ModelForm):
 
     class Meta:
         model = Ad
-        fields = ['title', 'text', 'picture']  # Picture is manual
+        fields = ['title', 'text',  'price', 'picture',]  # Picture is manual
 
     def clean(self) :
         cleaned_data = super().clean()
--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,10 +1,8 @@
 ##just finish adding the templates, step #3 is complete
-from ads.models import Ad
-from ads.forms import CreateForm
+from ads.models import Ad, Comment
+from ads.forms import CreateForm, CommentForm
 
 from django.views import View
-from django.views import generic
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.http import HttpResponse
@@ -21,10 +19,16 @@ class AdListView(OwnerListView):
 class AdDetailView(OwnerDetailView):
     model = Ad
     template_name = "ad_detail.html"
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
 class AdFormView(LoginRequiredMixin, View):
-    template = 'ads/ad_form.html'
-    success_url = reverse_lazy('ads')
+    template = "ad_form.html"
+    success_url = reverse_lazy("ads")
     def get(self, request, pk=None) :
         if not pk :
             form = CreateForm()
@@ -46,9 +50,9 @@ class AdFormView(LoginRequiredMixin, View):
             return render(request, self.template, ctx)
 
         # Adjust the model owner before saving
-        pic = form.save(commit=False)
-        pic.owner = self.request.user
-        pic.save()
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
         return redirect(self.success_url)
 
 class AdDeleteView(OwnerDeleteView):
@@ -56,9 +60,25 @@ class AdDeleteView(OwnerDeleteView):
     template_name = "ad_delete.html"
 
 def stream_file(request, pk) :
-    pic = get_object_or_404(Pic, id=pk)
+    pic = get_object_or_404(Ad, id=pk)
     response = HttpResponse()
     response['Content-Type'] = pic.content_type
     response['Content-Length'] = len(pic.picture)
     response.write(pic.picture)
     return response
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment_form = CommentForm(request.POST)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse_lazy('ad_detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "comment_delete.html"
+
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse_lazy('ad_detail', args=[ad.id])
